1f56cb232ce2b7b9f749957f87ffa4c3e2177b41
Yasuyuki Ageishi <age15h1@me.com>
2015-09-17 22:53:18 +0900

    merged jgb_data.py and jgb_data_process.py

--- jgb_data.py
+++ jgb_data.py
@@ -1,18 +1,27 @@
 '''update jgb data from mof web site'''
 
 import requests
+import pandas as pd
 
 
 url_cur = "http://www.mof.go.jp/english/jgbs/reference/interest_rate/jgbcme.csv"
 url_hist = "http://www.mof.go.jp/english/jgbs/reference/interest_rate/historical/jgbcme_all.csv"
 
 fn = 'hist_jgb.csv'
+new_fn = 'new_hist_jgb.csv'
 
+def process_jgb_data(fn, new_fn, verbose=False):
+    ''' delete rows contain "-" value '''
+    if verbose: print 'loading [%s]' % fn
+    jgb_df = pd.read_csv(fn, index_col=0)
+    new_jgb_df = jgb_df[jgb_df.apply(lambda x: not any(x == '-'), 1)]
+    new_jgb_df.to_csv('new_hist_jgb.csv', index=True)
+    if verbose: print 'write data to [%s]' % new_fn
 
 
 if __name__ == '__main__':
     # get historical jgb interest rate data from MOF japan
-    print '> downloading jgb data from [%s]' % url_hist
+    print '>>> downloading jgb data from [%s] ...' % url_hist
     r = requests.get(url_hist)
 
     sc = r.status_code
@@ -25,6 +34,10 @@ if __name__ == '__main__':
             f.write(r.text)
 
         print '[+] saved data successfly'
+
+        print 'processing "%s"' % fn
+        process_jgb_data(fn, new_fn)
+        print '[+] processing successfully done. file saved at "%s"' % new_fn
 
     else:
         print '[-] error statsu code [%d]' % sc
